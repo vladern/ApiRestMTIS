b1d47774bf6e380809bb4253824c3d5df904d038
vladern <vladys.3234@gmail.com>
2017-05-16 12:30:14 +0200

    añadido un get

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const port = process.env.PORT || 3001
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+
+app.get('/hola:name',(req,res)=>
+{
+    res.send({message:`Hola ${req.params.name}`})
+})
 app.listen(port,()=>
 {
     console.log(`API Rest corriendo en localhost:${port}`)
