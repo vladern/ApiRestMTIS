3c1296456ab92e80495d498acae659dd36c15de5
vladern <vladys.3234@gmail.com>
2017-05-17 14:08:40 +0200

    bd

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports =
 {
     puerto : process.env.PORT || 3001,
-    db : process.env.MONGODB || 'mongodb://localhost:27017/pasteleria'
+    db : process.env.MONGODB || 'mongodb://admin:admin@ds137191.mlab.com:37191/heroku_swh02g1n'
 }
