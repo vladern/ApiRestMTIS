0dea83f40ec398a486928dae3c62d1427e51461e
Vladyslav Kuchmenko <vladys.3234@gmail.com>
2017-12-09 14:40:14 +0100

    arreglado el bug del password

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,9 +24,9 @@ function signIn(req,res)
 {
     User.find({email: req.body.email},(err,user)=>
     {
-        if(err) return res.status(500).message({message: err})
-        if(!user) return res.status(404).message({message: 'no exiete el usuario'})
-
+        if(err) return res.status(500).send({message: err})
+        if(!user[0]) return res.status(404).send({message: 'no exiete el usuario'})
+        if(user[0].password !== req.body.password) return res.status(404).send({message: 'credenciales incorectas'})
         req.user=user
         res.status(200).send(
         {
--- models/user.js
+++ models/user.js
@@ -8,29 +8,10 @@ const UserSchema = new Schema
 ({
     email:{type:String,unique:true,lowercase:true},
     displayName:String,
-    password:{type:String,select:false},
+    password:{type:String},
     singupDate:{type:Date,default:Date.now},
     lastLogin: Date,
 })
 
-UserSchema.pre('save',(next)=>
-{
-    let user = this
-    //si la contraseña no se ha modificado
-    //if(!user.isModified('password')) return next()
-
-    bcrypt.genSalt(10,(err,salt)=>
-    {
-        if(err) return next(err)
-        //consigo el hash de la contraseña
-        bcrypt.hash(user.password,salt,null,(err,hash)=>
-        {
-            if(err) return next(err)
-            //guardo la contraseña ya hasheada
-            user.password=hash
-            next()
-        })
-    })
-})
 
 module.exports = mongoose.model('User',UserSchema)
